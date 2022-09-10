565cd5dacbab334e7689be5f5a57f3a69d7a10cb
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-09-10 14:25:53 +0100

    instalando o mocka chai

--- src/main.js
+++ src/main.js
@@ -1,2 +1,64 @@
+ 
 
- console.log("ssdd");
+
+//  class Animal{
+    
+//     constructor(kind, sound){
+//         this.kind = kind;
+//         this.sound = sound;
+//     }
+
+//     hello(){
+//         console.log(`${this.sound} I'm ${this.kind}`);
+//     }
+
+//     static info(){
+//         console.log("this is class to create a Animal");
+//     }
+
+
+//     get name(){
+//         return "this is my name"
+//     }
+
+//     set nickname(nick){
+//         this.nick = nick
+//     }
+//  }
+
+
+//  const ani = new Animal("PEDRO", "JDJKDJFK");
+
+
+//  ani.hello();
+
+
+// class Cabrito extends Animal{
+    
+//     constructor(kind, sound, pel){
+
+//         super(kind, sound);
+
+//         this.pel = pel;
+
+//     }
+
+    
+// }
+
+
+// const cab = new Cabrito("ffff","ddf","negra");
+
+
+// console.log(cab.pel);
+
+
+
+
+
+
+
+
+
+
+ 
